a5897c535be0ba171794f0a86e9c22efc1c36611
Piotr <piotr@fastmail.co.uk>
2020-04-13 21:38:37 +0100

    graphs on different site

--- espDHT.py
+++ espDHT.py
@@ -8,17 +8,18 @@ import time
 import network
 import dht
 
-blue_led = Pin(2, Pin.OUT, value=0)
-led = Pin(4, Pin.OUT, value=0)
+blue_led = Pin(2, Pin.OUT, value=1)
+led = Pin(4, Pin.OUT, value=1)
 online_led = Pin(5, Pin.OUT)
 
-SERVER = "m24.cloudmqtt.com"
+# SERVER = "m24.cloudmqtt.com"
+SERVER = "192.168.1.85"
 CLIENT_ID = 'ESP' #sprawdzic czy to ma znaczenie (duze/male litery)
-PORT = 17208
-USER = "dvukmvfa"
-PASSWORD = "JpfjsyzaE7Le"
+PORT = 1883
+# PORT = 17208
+# USER = "dvukmvfa"
+# PASSWORD = "JpfjsyzaE7Le"
 TOPIC = b"sensor"
-
 
 
 def do_connect():
@@ -28,25 +29,35 @@ def do_connect():
         print('connecting to network...')
         sta_if.active(True)
         sta_if.connect('ADSLPT-TR2355FF', '4B781E8D0B')
+        # print(sta_if.status())
         while not sta_if.isconnected():
             pass
-    print('network config:', sta_if.ifconfig())
+    # print('network config:', sta_if.ifconfig())
+    if sta_if.status() == 5:
+        return True
+    else:
+        return False
 
 
+
+
+def do_disconnect():
+    net_module = network.WLAN(network.STA_IF)
+    if net_module.isconnected():
+        net_module.disconnect()
+        if net_module.status() == 0:
+            print("disconnected from network")
 
 
 def blue_led_blink(num):
     for item in range(0,num):
-        blue_led.value(1)
-        time.sleep(0.05)
         blue_led.value(0)
         time.sleep(0.05)
+        blue_led.value(1)
+        time.sleep(0.05)
 
 
-
-#do_connect()
-
-sensor = dht.DHT22(Pin(18))
+sensor = dht.DHT22(Pin(12))
 
 def dht_readings():
     global temp, hum
@@ -59,6 +70,7 @@ def dht_readings():
         if (isinstance(temp, float) and isinstance(hum, float)) or (isinstance(temp, int) and isinstance(hum, int)):
           msg = (b'{0:3.1f},{1}'.format(temp, hum))
           hum = round(hum, 2)
+          print(msg)
           return(msg)
 
         else:
@@ -67,25 +79,31 @@ def dht_readings():
       return('Failed to read sensor.')
 
 
+# do_connect()
+#dht_readings()
 
 
-do_connect()
+def mqtt_publish(first_msg, second_msg):
+    # c = MQTTClient(CLIENT_ID, SERVER, PORT, USER, PASSWORD, keepalive=0)
+    c = MQTTClient(CLIENT_ID, SERVER, PORT, keepalive=0)
 
-
-def mqtt_publish():
-    c = MQTTClient(CLIENT_ID, SERVER, PORT, USER, PASSWORD, keepalive=0)
     c.connect()
-    c.publish(b'sensors/temperature', (b'{0:3.1f}'.format(temp)))
-    c.publish(b'sensors/humidity', (b'{0:3.1f}' .format(hum)))
+    c.publish(b'sensors/temperature', (b'{0:3.1f}'.format(first_msg)))
+    c.publish(b'sensors/humidity', (b'{0:3.1f}' .format(second_msg)))
     blue_led_blink(4)
     c.disconnect()
 
 
 def main(period):
     while True:
-        dht_readings()
-    
-        mqtt_publish()
+        if do_connect() == True:
+            dht_readings()
+            mqtt_publish(temp,hum)
+            do_disconnect()
+        else:
+            print("can't connect to network right now")
+            time.sleep(60)
         time.sleep(period)
 
-main(15)
+
+main(300)
